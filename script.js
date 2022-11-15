17cff0ea6c7f0b2bee771758b51be7c0600c7be2
srkans <serkansacma@hotmail.com>
2022-11-15 17:26:09 +0300

    create remove element git add . EVENTS

--- script.js
+++ script.js
@@ -22,48 +22,85 @@ function playRound(playerSelection) {
     
     computerSelection = getComputerChoice();
 
-    if(playerSelection===computerSelection) {
+    if (userScore<5 && computerScore<5) {
+        if(playerSelection===computerSelection) {
 
-        return console.log("Tie");
+            result="Tie";
+        }
+
+        if(playerSelection=="rock"&&computerSelection=="paper") {
+            computerScore +=1;
+            result="You Lost! Paper beats Rock.";
+        }
+        else if(playerSelection=="rock"&&computerSelection=="scissors") {
+
+            userScore+=1;
+            result="You Win! Rock beats Scissors";
+        }
+
+        if(playerSelection=="paper"&&computerSelection=="rock") {
+            userScore+=1;
+            result="You Win! Paper beats Rock.";
+        }
+        else if(playerSelection=="paper"&&computerSelection=="scissors") {
+            computerScore +=1;
+            result="You Lost! Scissors beats Paper";
+        }
+
+        if(playerSelection=="scissors"&&computerSelection=="rock") {
+            computerScore +=1;
+            result = "You Lost! Rock beats Scissors.";
+        }
+        else if(playerSelection=="scissors"&&computerSelection=="paper") {
+            userScore+=1;
+            result ="You Win! Scissors beats Paper";
+
+        }
     }
-
-    if(playerSelection=="rock"&&computerSelection=="paper") {
-        computerScore +=1;
-        return console.log("You Lost! Paper beats Rock.");
-    }
-    else if(playerSelection=="rock"&&computerSelection=="scissors") {
-
-        userScore+=1;
-        return console.log("You Win! Rock beats Scissors");
-    }
-
-    if(playerSelection=="paper"&&computerSelection=="rock") {
-        userScore+=1;
-        return console.log("You Win! Paper beats Rock.");
-    }
-    else if(playerSelection=="paper"&&computerSelection=="scissors") {
-        computerScore +=1;
-        return console.log("You Lost! Scissors beats Paper");
-    }
-
-    if(playerSelection=="scissors"&&computerSelection=="rock") {
-        computerScore +=1;
-        return console.log("You Lost! Rock beats Scissors.");
-    }
-    else if(playerSelection=="scissors"&&computerSelection=="paper") {
-        userScore+=1;
-        return console.log("You Win! Scissors beats Paper");
-
-    }
-
-
 
 }
 
 function updateScore() {
      scoreComputer.textContent = computerScore;
      scoreUser.textContent = userScore;
+     roundResult.textContent = result;
 }
+
+
+function endGame () {
+
+    if (userScore==5) {
+
+        resMessage.innerHTML = "You win the Game!!! Do you wanna play again?" 
+        restartButton.innerText = "OK!";
+
+    
+    }
+    else if(computerScore==5) {
+        resMessage.innerHTML = "You lost the Game!!! Do you wanna play again?" 
+        restartButton.innerText = "OK!";
+ 
+    }
+
+}
+
+function restartGame() {
+
+    userScore = 0;
+    computerScore = 0;
+    document.body.removeChild(resMessage);
+    document.body.removeChild(restartButton);
+    resMessage.innerHTML ="";
+    restartButton.innerText = "";
+    result = "";
+    document.body.appendChild(resMessage);
+    document.body.appendChild(restartButton);
+
+}
+
+
+
+
 
 //Interface for DOM
 const rockBtn = document.getElementById("rock-btn");
@@ -71,6 +108,10 @@ const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
 const scoreComputer = document.querySelector(".cscore");
 const scoreUser = document.querySelector(".uscore");
+const roundResult = document.querySelector("#round");
+const resMessage = document.createElement("p");
+const restartButton = document.createElement("button");
+
 
 
 
@@ -80,6 +121,17 @@ scissorsBtn.addEventListener("click", function(){playRound("scissors");});
 rockBtn.addEventListener("click", function(){updateScore();});
 paperBtn.addEventListener("click", function(){updateScore();});
 scissorsBtn.addEventListener("click", function(){updateScore();});
+
+
+ rockBtn.addEventListener("click", function(){endGame();});
+ paperBtn.addEventListener("click", function(){endGame();});
+ scissorsBtn.addEventListener("click", function(){endGame();});
+
+
+document.body.appendChild(resMessage);
+document.body.appendChild(restartButton);
+
+restartButton.addEventListener("click",function(){restartGame(),updateScore()});
 
     
   
